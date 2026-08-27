80e766fbeb6eb86f1308a5216f77d58595ef4e1d
Shaan-somaiah <shaansomaiah@gmail.com>
2026-08-27 19:48:29 +0530

    Lib to run remote and local commands

---
+++ cmd_runner.py
@@ -0,0 +1,51 @@
+import subprocess
+import sys
+from abc import ABC, abstractmethod
+
+
+## ICommandRunner provides an interface to call both local command runner and remote command runner (via ssh)
+## Force all child classes to implement atleast RunCmd() via abstractmethod
+class ICmdRunner(ABC):
+
+    @abstractmethod
+    def RunCmd(self, command):
+        pass
+
+
+## Run commands on the machine which invokes the methods
+class LocalCmdRunner(ICmdRunner):
+
+## No need for explictl constructor
+    def RunCmd(self, command):
+
+        return subprocess.run (
+            command,
+            capture_output=True,
+            text=True,
+            check=True
+        )
+
+
+## Run commands on a remote machine via ssh
+class RemoteCmdRunner(ICmdRunner):
+
+    ssh_exec = "/usr/bin/ssh"
+
+    def __init__(self, remote_host, username = "shaan"):
+
+        if remote_host == "":
+            print(f"Remote host not supplied, panic!")
+            sys.exit(3)
+
+        self.remote_host = remote_host
+        self.username = username
+
+
+    def RunCmd(self, command):
+
+        return subprocess.run (
+            [ self.ssh_exec, f"{self.username}@{self.remote_host}", *command ],
+            capture_output=True,
+            text=True,
+            check=True
+        )
